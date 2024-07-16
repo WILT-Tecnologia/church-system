022c4cbd31a8213b9c49bca55c19c4bc3375b737
dandandw <wdanglanescs@gmail.com>
2024-07-16 19:45:59 -0400

    feature: Crud da Company

---
+++ backend/app/Http/Resources/CompanyResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CompanyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'cnpj' => $this->cnpj,
+            'cep' => $this->cep,
+            'street' => $this->street,
+            'number' => $this->number,
+            'complement' => $this->complement,
+            'district' => $this->district,
+            'city' => $this->city,
+            'state' => $this->state,
+            'country' => $this->country
+        ];
+    }
+}
