12ddc58d9c3fb8314588acbf20812b71b84f68d4
dandandw <wdanglanescs@gmail.com>
2024-07-24 19:08:09 -0400

    add route Church

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,4 +11,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
     Route::post('/logout/{user}', [loginController::class, 'logout']);
     Route::apiResource('companys', \App\Http\Controllers\Api\CompanyController::class);
+    Route::apiResource('churches', \App\Http\Controllers\ChurchController::class);
 });
