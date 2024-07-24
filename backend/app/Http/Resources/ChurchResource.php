226e90ff5df244565ecaf6de1f6711440aabf1eb
dandandw <wdanglanescs@gmail.com>
2024-07-24 18:14:32 -0400

    create Church Resource

---
+++ backend/app/Http/Resources/ChurchResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ChurchResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->uuid,
+            'name' => $this->name,
+            'email' => $this->email,
+            'cnpj' => $this->cnpj,
+            'cep' => $this->cep,
+            'street' => $this->street,
+            'number' => $this->number,
+            'complement' => $this->complement,
+            'district' => $this->district,
+            'city' => $this->city,
+            'state' => $this->state,
+            'country' => $this->country,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted' => $this->deleted_at
+        ];
+    }
+}
