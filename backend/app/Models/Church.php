d78f4f0b97574a1ca28e07da73a0fa232e4e8b77
dandandw <wdanglanescs@gmail.com>
2024-07-24 18:06:32 -0400

    create model Church

---
+++ backend/app/Models/Church.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Church extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    use HasUuids;
+
+    protected $table = 'churches';
+
+    protected $primaryKey = 'uuid';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'cnpj',
+        'cep',
+        'street',
+        'number',
+        'complement',
+        'district',
+        'city',
+        'state',
+        'country',
+    ];
+
+}
