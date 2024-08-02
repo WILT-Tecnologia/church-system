96f39cbce279ca877e326f376023a3833b59be19
dandandw <wdanglanescs@gmail.com>
2024-08-02 14:52:00 -0400

    BACKEND: add to person route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,6 +10,6 @@ Route::post('teste', [LoginController::class, 'teste']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
     Route::post('/logout/{user}', [loginController::class, 'logout']);
-    Route::apiResource('companys', \App\Http\Controllers\Api\CompanyController::class);
+    Route::apiResource('persons', \App\Http\Controllers\Api\PersonController::class);
     Route::apiResource('churches', \App\Http\Controllers\ChurchController::class);
 });
