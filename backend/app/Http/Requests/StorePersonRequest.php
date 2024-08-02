77f54d866dc3608327da95f0f4fd544985bcfaa0
dandandw <wdanglanescs@gmail.com>
2024-08-02 14:31:32 -0400

    BACKEND: creating the person store and updade request

---
+++ backend/app/Http/Requests/StorePersonRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\SexEnum;
+use App\Rules\Cpf;
+use App\Rules\Phone;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+
+class StorePersonRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'image' => ['nullable'],
+            'name' => ['requied'],
+            'cpf' => ['required', 'numeric', 'unique:persons,cpf', new Cpf()],
+            'birt_date' => ['nullable', 'date'],
+            'phone_one' => ['nullable', 'numeric', new Phone()],
+            'sex' => ['required', new Enum(SexEnum::class)],
+            'cep' => ['nullable', 'numeric'],
+            'street' => ['nullable', 'required_with:cep'],
+            'number' => ['nullable', 'numeric', 'required_with:cep'],
+            'complement' => ['nullable'],
+            'district' => ['nullable', 'required_with:cep'],
+            'city' => ['nullable', 'required_with:cep'],
+            'state' => ['nullable', 'required_with:cep'],
+            'country' => ['nullable', 'required_with:cep'],
+        ];
+    }
+}
