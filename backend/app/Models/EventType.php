8922e0ce2038657b9af8a2cfe6499ac8f3afbd3d
dandandw <wdanglanescs@gmail.com>
2024-08-07 20:23:59 -0400

    BACKEND: creating the event type model

---
+++ backend/app/Models/EventType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class EventType extends Model
+{
+    use HasFactory;
+    use HasUuids;
+    use SoftDeletes;
+
+    protected $table = 'event_types';
+
+    protected $fillable = [
+        'id',
+        'name',
+        'description',
+        'status',
+    ];
+    
+}
