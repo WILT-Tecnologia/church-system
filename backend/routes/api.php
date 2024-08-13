747d94a39ce99ddf8a87b1c85da22e2763d2a7cd
dandandw <wdanglanescs@gmail.com>
2024-08-12 21:49:47 -0400

    BACKEND: fixing remove route token

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('teste', [LoginController::class, 'teste']);
 
-Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
+Route::prefix('admin')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
     Route::post('/logout/{user}', [loginController::class, 'logout']);
     Route::apiResource('persons', \App\Http\Controllers\Api\PersonController::class);
