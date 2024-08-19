376a03f22e971473f3b96f96a671dfa8ac81199f
dandandw <wdanglanescs@gmail.com>
2024-08-19 18:08:57 -0400

    BACKEND: creating the store and update request of the member

--- backend/database/migrations/2024_08_15_190933_create_members_table.php
+++ backend/database/migrations/2024_08_15_190933_create_members_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('rg')->nullable();
             $table->string('issuing_body')->nullable();
             $table->enum('civil_status', ['Solteiro(a)', 'Casado(a)', 'Separado(a)', 'Divorciado(a)', 'Viúvo(a)']);
-            $table->string('nacionality');
+            $table->string('nacionality')->default('Brasileira');
             $table->string('naturalness');
             $table->enum('color_race', ['Branca', 'Preta', 'Parda', 'Amarela', 'indÍgena']);
             $table->enum('formation', ['Analfabeto', 'Lê e Escreve', 'Ensino Fundamental Incompleto',
