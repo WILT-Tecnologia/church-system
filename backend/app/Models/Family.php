eb4d6c2bf3ab5cdd5fb31e4bda687f4d1531ec71
dandandw <wdanglanescs@gmail.com>
2024-09-03 16:51:36 -0400

    BACKEND: criação do model de Family

---
+++ backend/app/Models/Family.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Family extends Model
+{
+    use HasFactory;
+    use HasUuids;
+    use SoftDeletes;
+
+    protected $table = 'families';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'id',
+        'member_id',
+        'person_id',
+        'name',
+        'kinship'
+    ];
+
+    public function member(): BelongsTo
+    {
+        return $this->belongsTo(Member::class);
+    }
+
+    public function person(): BelongsTo
+    {
+        return $this->belongsTo(Person::class);
+    }
+}
