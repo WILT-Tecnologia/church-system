4a5573a3b1a8ffec40781c65d3faf79702656660
dandandw <wdanglanescs@gmail.com>
2024-10-17 21:31:13 -0400

    BACKEND: criação do resource de historico do membro

---
+++ backend/app/Http/Resources/HistMemberResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HistMemberResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'table_name' => $this->table_name,
+            'before_situation' => $this->before_situation,
+            'after_situation' => $this->after_situation,
+            'change_date' => $this->change_date,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
