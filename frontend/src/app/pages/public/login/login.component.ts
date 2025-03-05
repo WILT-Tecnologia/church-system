152383338262359bdcb7c87a247d15ecfb7b78b8
Peixola <thiagopersch@gmail.com>
2025-03-05 14:44:12 -0400

    backend and frontend: add fields start and end times and dates

--- frontend/src/app/pages/public/login/login.component.ts
+++ frontend/src/app/pages/public/login/login.component.ts
@@ -37,11 +37,6 @@ import { ValidationService } from '../../../services/validation/validation.servi
     MatIconModule,
     CommonModule,
     ReactiveFormsModule,
-    
-// TODO: `HttpClientModule` should not be imported into a component directly.
-// Please refactor the code to add `provideHttpClient()` call to the provider list in the
-// application bootstrap logic and remove the `HttpClientModule` import from this component.
-HttpClientModule,
     ColumnComponent,
     ActionsComponent,
   ],
