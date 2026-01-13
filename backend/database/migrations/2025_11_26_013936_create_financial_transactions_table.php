ffead05256437c844a4c72234400d6c60dd24659
dandandw <wdanglanescs@gmail.com>
2026-01-12 20:41:24 -0400

    BACKEND: Criação da migration da tabela de lançamentos financeiros

---
+++ backend/database/migrations/2025_11_26_013936_create_financial_transactions_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('financial_transactions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('church_id');
+            $table->enum('entry_exit', ['entrada', 'saida']);
+            $table->enum('customer_supplier', ['membro', 'fornecedor', 'pessoa']);
+            $table->uuid('member_id')->nullable();
+            $table->uuid('supplier_id')->nullable();
+            $table->string('person_name')->nullable();
+            $table->text('description')->nullable();
+            $table->uuid('cat_financial_id');
+            $table->enum('payment',['pix', 'dinheiro', 'boleto', 'crédito', 'débito', 'cheque']);
+            $table->decimal('amount', 8, 2);
+            $table->decimal('discount', 8, 2)->nullable();
+            $table->decimal('amount_discount', 8, 2)->nullable();
+            $table->date('payment_date');
+            $table->string('receipt')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('church_id')->references('id')->on('churches')->onDelete('cascade');
+            $table->foreign('member_id')->references('id')->on('members')->onDelete('cascade');
+            $table->foreign('supplier_id')->references('id')->on('suppliers')->onDelete('cascade');
+            $table->foreign('cat_financial_id')->references('id')->on('fincancial_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('financial_transactions');
+    }
+};
